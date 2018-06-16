08d2d9e4768d6e4ce05126ec82b142cdd5aea498
luynt <nguyenthanhluynd@gmail.com>
2018-06-16 17:48:46 +0700

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,6 @@ WORKDIR $GOPATH/src/git.hocngay.com/techmaster/service-complier
 
 RUN go build -o service-complier .
 
-RUN  apk-install sudo
-
 RUN chmod +x ./installdocker.sh && ./installdocker.sh
 
 CMD ["./service-complier"]
--- installdocker.sh
+++ installdocker.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-sudo sed -e 's;^#http\(.*\)/v3.6/community;http\1/v3.6/community;g' \
+sed -e 's;^#http\(.*\)/v3.6/community;http\1/v3.6/community;g' \
      -i /etc/apk/repositories
-sudo apk update
-sudo apk add docker
+apk update
+apk add docker
 
 memb=$(grep "^docker:" /etc/group | sed -e 's/^.*:\([^:]*\)$/\1/g')
 [ "${memb}x" = "x" ] && memb=${USER} || memb="${memb},${USER}"
-sudo sed -e "s/^docker:\(.*\):\([^:]*\)$/docker:\1:${memb}/g" -i /etc/group
 
-sudo rc-update add docker
-sudo reboot
+sed -e "s/^docker:\(.*\):\([^:]*\)$/docker:\1:${memb}/g" -i /etc/group
+
+rc-update add docker
