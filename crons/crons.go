07db8adbf1858052bba344949d01c06f2f61b576
luynt <nguyenthanhluynd@gmail.com>
2018-06-21 16:30:45 +0700

    update crons

--- crons/crons.go
+++ crons/crons.go
@@ -74,6 +74,9 @@ func (t *Cron) CheckCPU() {
 		percentRam, _ := info.MemoryPercent()
 		createdTime, _ := info.CreateTime()
 		duration := time.Since(time.Unix(createdTime, 0))
+
+		logrus.Infof(" Info:  Name %v UserName %v  PercentCPU %v RamUse %v \n", name, userName, percent,percentRam)
+
 		if  percent >= t.MaxCPU || percentRam >= t.MaxRAM || duration >= t.ProcessDur {
 			logrus.Infof(" Tìm thấy Process:  PID %v Name %v UserName %v  Percent %v \n", info.Pid, name, userName, percent)
 			info.Kill()
